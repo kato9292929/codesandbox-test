9a7559db7cab3875203a0286752568f72beb4e25
kato9292929 <76525458+kato9292929@users.noreply.github.com>
2021-10-02 17:10:28 +0900

    3 3

--- src/index.js
+++ src/index.js
@@ -83,3 +83,34 @@
 // const arr8 = arr4;
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapやlitterを使った配列の処理
+ */
+
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index +1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name) => console.log(`${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// })
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
