9a1f8cd303cf0b5c88313ba2a9f124efaacfad61
kato9292929 <76525458+kato9292929@users.noreply.github.com>
2021-10-02 15:00:27 +0900

    1 1

--- src/index.js
+++ src/index.js
@@ -12,3 +12,60 @@
 // //var変数は再宣言可能
 // var val1 = "var変数を再宣言"
 // console.log(val1);
+
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+
+//  const myProfile = {
+//    name: "じゃけぇ",
+//    age:28,
+//  };
+
+// const message1 = '名前は${myProfile.name}です。年齢は${myProfile.age}歳です。';
+// console.log(message1);
+
+// const { name, age} = myProfile
+// const message2 = '名前は${name}です。年齢は${age}歳です。';
+// console.log(message2);
+
+// const myProfile = ['じゃけぇ' , 28];
+
+// const message3 = '名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。'
+// console.log(message3);
+
+// const [name, age] = myProfile
+// constt message4 = '名前は${name}です。年齢は${age}です。'
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん!`);
+// sayHello("じゃけぇ");
+
+// /**
+//  * スペレッド構文 ...
+//  */
+// //配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+
+// sumFunc(...arr1);
+
+//まとめる
+
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
